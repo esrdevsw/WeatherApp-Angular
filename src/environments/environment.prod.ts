92a0be7357e144c7aa4688adc9950be9bb3a6762
Edivagner Ribeiro <esrdevsw@gmail.com>
2023-04-24 12:41:58 +0100

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+//export const environment = {
+//  production: true
+  
+//};
+
 export const environment = {
-  production: true
+  production: true,
+  weatherAPIbaseURL: 'https://visual-crossing-weather.p.rapidapi.com/forecast?',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'visual-crossing-weather.p.rapidapi.com',
+
+  XRapidAPIKeytHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '1127a16685msh36a49afcf28f7ecp1f3d90jsnee46154373b8',
+
 };
